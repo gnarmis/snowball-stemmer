ee35c7d33365745f4bc7e172b33d1b7b7969db18
Gursimran Singh <g13singh@gmail.com>
2012-07-05 21:25:28 -0500

    added :aot

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject org.clojars.gnarmis/snowball-stemmer "0.1.0-SNAPSHOT"
+(defproject org.clojars.gnarmis/snowball-stemmer "0.1.1"
   :description "Wrapper around the Snowball stemmer algorithm."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :source-path "src/clj"
-  :java-source-path "src/java")
+  :java-source-path "src/java"
+  :aot [stemmer.snowball])
